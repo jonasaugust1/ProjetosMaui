053e707063289b0ac9ea9720b9dc7e7569565914
Jonas Augusto <95719426+jonasaugust1@users.noreply.github.com>
2023-10-04 20:21:42 -0300

    Implementing GenerateLuckNumbers Logic

--- AppNumeroDaSorte/MainPage.xaml.cs
+++ AppNumeroDaSorte/MainPage.xaml.cs
@@ -13,5 +13,29 @@ public partial class MainPage : ContentPage
 
         AppName.IsVisible = false;
 		ContainerLuckNumbers.IsVisible = true;
+
+		SortedSet<int> luckNumbers = GenerateLuckNumbers();
+
+		NumberOne.Text = luckNumbers.ElementAt(0).ToString("D2");
+        NumberTwo.Text = luckNumbers.ElementAt(1).ToString("D2");
+        NumberThree.Text = luckNumbers.ElementAt(2).ToString("D2");
+        NumberFour.Text = luckNumbers.ElementAt(3).ToString("D2");
+        NumberFive.Text = luckNumbers.ElementAt(4).ToString("D2");
+        NumberSix.Text = luckNumbers.ElementAt(5).ToString("D2");
+    }
+
+	private SortedSet<int> GenerateLuckNumbers()
+	{
+		SortedSet<int> luckNumbers = new();
+		Random random = new();
+
+		while(luckNumbers.Count < 6)
+		{
+			int luckNumber = random.Next(1, 60);
+
+            luckNumbers.Add(luckNumber);
+        }
+		
+		return luckNumbers;
 	}
 }
